0a5da3c398c0535ee6944f05feb1ee8d1baca67a
Gabriel432135 <gabriel432135@hotmail.com>
2026-04-28 18:54:59 -0300

    preparação e build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 /*
 * Copyright (C) 2023 The Android Open Source Project
 *
@@ -49,9 +51,12 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
     }
     buildFeatures {
         compose = true
