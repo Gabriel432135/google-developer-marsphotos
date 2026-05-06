9945aa98a5a7904a1ebba3e8a563cdc8073747ad
Gabriel432135 <gabriel432135@hotmail.com>
2026-05-06 20:33:58 -0300

    when marsUiState

--- app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -31,7 +31,7 @@ sealed interface MarsUiState {
 }
 class MarsViewModel : ViewModel() {
     /** The mutable State that stores the status of the most recent request */
-    var marsUiState: String by mutableStateOf("")
+    var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
 
     /**
@@ -49,9 +49,9 @@ class MarsViewModel : ViewModel() {
         viewModelScope.launch {
             try {
                 val listResult = MarsApi.retrofitService.getPhotos()
-                marsUiState = listResult
+                marsUiState = MarsUiState.Success(listResult)
             }catch(e: IOException){
-
+                marsUiState = MarsUiState.Error
             }
         }
     }
