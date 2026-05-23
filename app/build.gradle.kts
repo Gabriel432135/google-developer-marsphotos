c74b3557ed856373cf0158833ba94696c1c67775
Gabriel432135 <gabriel432135@hotmail.com>
2026-05-22 22:10:00 -0300

    FakeDataSource e FakeMarsApiService para teste

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ android {
 dependencies {
 
     // Import the Compose BOM
-    implementation(platform("androidx.compose:compose-bom:2026.05.00"))
+    implementation(platform("androidx.compose:compose-bom:2026.05.01"))
     implementation("androidx.activity:activity-compose:1.13.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
@@ -89,6 +89,9 @@ dependencies {
     // Retrofit with Kotlin serialization Converter
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.okhttp3:okhttp:5.3.2")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.11.0")
 
     // Kotlin serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
